d395f8fa5381613370eb2a10c97ab87c7a8af33e
남아영 <ejr3389@naver.com>
2022-01-04 21:49:37 +0900

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,11 @@
+public class datatype{
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six");//string
+		
+		System.out.println("6"); //string 6
+		System.out.println(6+6); //12
+		System.out.println("6"+"6"); //66
+		System.out.println("1111".length());
+	}
+}
