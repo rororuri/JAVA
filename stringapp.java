eaeae2d51472c14ab1eba0606555cda291e20ee7
남아영 <ejr3389@naver.com>
2022-01-06 22:29:31 +0900

    Create stringapp.java

---
+++ stringapp.java
@@ -0,0 +1,15 @@
+
+public class stringapp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //String
+		System.out.println('H'); //character
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "world");
+       System.out.println("Hello \nWorld"); //New line
+       System.out.println("Hello \"world\"");
+	}
+
+}
